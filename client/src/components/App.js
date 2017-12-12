0ecd9745bf75d7741a9a6cfaccdd32866254650b
joemcadams <joseph.mcadams@mu.edu>
2017-12-11 18:39:15 -0600

    fixed updated relative file paths

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider' // configures the default styling of material-ui 
                                                                    // components further down the component tree
-import { NavBar } from '../components/NavBar'
-import { LoginPage } from '../components/LoginPage'
-import { MenuPage } from '../containers/MenuPage'
-import { RestaurantCard } from '../components/RestaurantCard'
-import { RestaurantPage } from '../containers/RestaurantPage'
+import { NavBar } from './NavBar'
+import { LoginPage } from './LoginPage'
+import { MenuPage } from './menu/MenuPage'
+import { RestaurantCard } from './restaurant/RestaurantCard'
+import { RestaurantPage } from './restaurant/RestaurantPage'
 
 export const App = () => (
-    <MuiThemeProvider>
-        <NavBar/>
-        <LoginPage/>
-        <MenuPage/>
-        <RestaurantPage />
-    </MuiThemeProvider>
+    <div>
+        <MuiThemeProvider>
+            <NavBar/>
+            <LoginPage/>
+            <MenuPage/>
+            <RestaurantPage />
+        </MuiThemeProvider>
+    </div>
 )
