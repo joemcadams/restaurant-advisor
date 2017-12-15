197b414fb741e15b8f3e7e4e7c2cbc9b1d1dff64
joemcadams <joseph.mcadams@mu.edu>
2017-12-14 22:10:44 -0600

    MONGO FUCKING WORKS NOW

--- server/app.js
+++ server/app.js
@@ -1,27 +1,10 @@
 const express = require('express')
-const Mongoose = require('mongoose')
-const Schemas = require('./db/schemas')
-const mockData = require('./mockData')
+const dbService = require('./db/databaseService')
 const app = express()
 
-const DB_PORT = 27017
-const CONNECTION_URL = `mongodb://admin:fuckmarquette>@ds137686.mlab.com:37686/restaurant-advisor`
- 
-const handleConnectionError = err => { throw `Unable to connect to Mongo instance @ ${CONNECTION_URL}:\n${JSON.stringify(err)}` }
-
-Mongoose.connect(CONNECTION_URL, (err) => {
-    if (!err) console.log(`Successfully connected to mongo instance @ ${CONNECTION_URL}!\n`)
-}).then(db => {
-    mockData.restaurants.forEach(restaurant => restaurant.save(err => { 
-        if (!err) console.log(`Successfully saved Restaurant ${restaurant.name}`)
-        else throw `Unable to save object ${restaurant.name} to db`
-    }))
-
-    app.get('/', (req, res) => res.send("Go to localhost:3000"))
-    
-    app.get('/Restaurants', (req, res) => {
-        Schemas.restaurant.find({}, (err, results) => res.send(results))
-    })
+dbService.setup()
+app.get('/Restaurants', (req, res) => {
+    dbService.getAllRestaurants(res)
 })
 
 
--- server/db/tables.js
+++ server/db/tables.js
@@ -1,10 +1,68 @@
-module.exports.default = {
-    TABLES: {
-        restaurant: "Restaurant",
-        customer: "Customer",
-        review: "Review",
-        food: "Food",
-        menu: "Menu",
-        order: "Order"
-    }
+/**
+ * Table schema:
+ * 
+ * Restaurant: {
+ *  _id: ObjectId,
+ *  name: String,
+ *  phone: String,
+ *  email: String,
+ *  hours: String,
+ *  diningType: String,
+ *  streetNo: Number,
+ *  streetName: String,
+ *  city: String,
+ *  state: String,
+ *  zip: Number,
+ *  priceRange: String,
+ *  offersDelivery: Boolean,
+ *  outdoorSeating: Boolean,
+ *  reviews: [{
+ *      customer: { email: String },
+ *      title: String,
+ *      description: String,
+ *      date: Date,
+ *      xzrating: Number
+ *  },...],
+ *  orders: [{
+ *      date: Date,
+ *      isDelivery: boolean,
+ *      isTakeOut: boolean,
+ *      order: [{
+ *          food: {
+ *              name: String,
+ *              category: String,
+ *              description: String
+ *              price: Number,
+ *              quantity: Number
+ *          }
+ *      },...]
+ *  }]
+ *  menu: {
+ *      food [{
+ *          id: ObjectId,
+ *          name: String,
+ *          category: String,
+ *          description: String
+ *          price: Number
+ *      },...]
+ *  }
+ * }
+ * 
+ * Customer: {
+ *  email: String,
+ *  fName: String,
+ *  mInit: String,
+ *  lName: String,
+ *  password: String,
+ *  streetNo: String,
+ *  streetName: String,
+ *  city: String,
+ *  state: String,
+ *  zip: Number
+ * }
+ */
+
+module.exports = {
+    restaurant: "Restaurant",
+    customer: "Customer"
 }
